c3f1641c8adf4ca951a838721186221604c20f84
davidbwetzel <david@wetzel-soyster.net>
2024-01-17 21:49:19 +0000

    Edit script

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // add your P5.js code here on the next line
 function setup(){
+    createCanvas(300, 200);
+    background(200);
     fill(0);
-    ellipse(50, 50, 50, 50)
+    ellipse(90, 50, 50, 50)
   }
